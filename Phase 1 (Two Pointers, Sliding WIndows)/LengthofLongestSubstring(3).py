d729b5dd0186cd6f8984deac4e24b9de0d8aa8c1
Tkota10 <tkota0910@gmail.com>
2023-06-06 17:46:05 -0500

    More Sliding Window Problems

---
+++ Phase 1 (Two Pointers, Sliding WIndows)/LengthofLongestSubstring(3).py
@@ -0,0 +1,31 @@
+
+# Correct Solution
+
+while end < len(s):
+        if s[end] not in unique_chars:
+            unique_chars.add(s[end])
+            templength += 1
+            longlength = max(longlength, templength)
+            end += 1
+        else:
+            unique_chars.remove(s[start])
+            start += 1
+            templength -= 1
+
+    return longlength
+
+
+#My Original Solution (Passed a third of the testcases)
+
+        while end < len (s):
+            if s[end] in unique_chars:
+                longlength = max (longlength, templength)
+                start = end
+                unique_chars.clear()
+                unique_chars.add(s[start])
+                templength = 1
+            else:
+                templength+=1
+                unique_chars.add(s[end])
+            end +=1
+        return longlength
