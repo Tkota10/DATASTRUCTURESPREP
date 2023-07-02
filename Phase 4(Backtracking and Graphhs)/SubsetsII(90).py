ba4012c0555728370a62f440e36549be26de53bd
Tkota10 <tkota0910@gmail.com>
2023-07-02 17:56:57 -0500

    Anotehr problem

---
+++ Phase 4(Backtracking and Graphhs)/SubsetsII(90).py
@@ -0,0 +1,50 @@
+#my Initial solution
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums = sorted(nums)
+
+
+        def backtrack(curr_set, start):
+            if curr_set[:] not in res:
+                res.append(curr_set[:])
+            for i in range(start, len(nums)):
+                curr_set.append(nums[i])
+                backtrack(curr_set, i + 1)
+                curr_set.pop()
+
+        backtrack([], 0)
+        return res
+
+
+
+
+#Best Solution:
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums = sorted(nums)
+
+        def backtrack(curr_set, start):
+            res.append(curr_set[:])  # Always append the current subset
+            prev = None  # Track the previously chosen element
+
+            for i in range(start, len(nums)):
+                if nums[i] != prev:
+                    curr_set.append(nums[i])
+                    backtrack(curr_set, i + 1)
+                    curr_set.pop()
+                    prev = nums[i]
+
+        backtrack([], 0)
+        return res
