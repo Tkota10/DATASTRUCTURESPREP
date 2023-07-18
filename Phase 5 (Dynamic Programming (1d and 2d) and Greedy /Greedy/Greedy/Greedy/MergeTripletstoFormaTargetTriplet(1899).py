c14f7fc0b1956235edb19829f92ead56dba9c82c
Tkota10 <tkota0910@gmail.com>
2023-07-18 17:47:04 -0500

    More greedy

---
+++ Phase 5 (Dynamic Programming (1d and 2d) and Greedy /Greedy/Greedy/Greedy/MergeTripletstoFormaTargetTriplet(1899).py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mergeTriplets(self, triplets, target):
+        """
+        :type triplets: List[List[int]]
+        :type target: List[int]
+        :rtype: bool
+        """
+        finalset = set()
+        for trip in triplets:
+            if trip[0] > target[0] or trip[1] > target[1] or trip[2] > target[2]: #Don't need to consider the triplets that are above the max
+                continue
+            for i, v in enumerate(trip):
+                if v == target[i]:
+                    finalset.add(i)
+
+        return len(finalset) == 3
