489c8f8e6588e980d8c139510cc1799b43e5404f
Tkota10 <tkota0910@gmail.com>
2023-07-23 12:57:23 -0500

    Better Dp problem

--- Phase 5 (Dynamic Programming (1d and 2d) and Greedy /Greedy/Greedy/Greedy/1d DP/LongestPalindromicSubstring(5).py
+++ Phase 5 (Dynamic Programming (1d and 2d) and Greedy /Greedy/Greedy/Greedy/1d DP/LongestPalindromicSubstring(5).py
@@ -1,32 +1,36 @@
 class Solution(object):
-    def longest_palindromic_substring(s):
-        n = len(s)
-        if n < 2:
-            return s
+    def longestPalindrome(self, s):
+        res = ""
+        resLen = 0
 
-        dp = [[False] * n for _ in range(n)]
-        start = 0
-        max_len = 1
+        #Starting from the center and expanding outwards
 
-        # All substrings of length 1 are palindromes
-        for i in range(n):
-            dp[i][i] = True
 
-        # Check substrings of length 2
-        for i in range(n - 1):
-            if s[i] == s[i + 1]:
-                dp[i][i + 1] = True
-                start = i
-                max_len = 2
 
-        # Check substrings of length > 2
-        for length in range(3, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
-                if s[i] == s[j] and dp[i + 1][j - 1]:
-                    dp[i][j] = True
-                    if length > max_len:
-                        start = i
-                        max_len = length
+        #We could use a queue; 
 
-        return s[start:start + max_len]
+        for i in range(len(s)):
+
+            #odd length
+
+            l, r = i, i
+
+            #Starting from thhe center (i) and then expanding outwards
+
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l: r + 1] #This how you get the string from l to r
+                    resLen = r - l + 1
+                l -= 1
+                r += 1 # Further expanding outwards
+
+            l, r = i, i + 1
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l: r + 1] #This how you get the string from l to r
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        return res
