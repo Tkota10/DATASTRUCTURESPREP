6f3788beb13ebce7a75e804dcf62afdabd59b69c
Tkota10 <tkota0910@gmail.com>
2023-07-27 16:53:07 -0500

    Cool String question

---
+++ Phase 1 (Two Pointers, Sliding WIndows)/Array Questions/String questions/String Questions/String Questions/PrintVertically(1324).py
@@ -0,0 +1,25 @@
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        s = s.split()
+        maxlen = 0
+        for i in s:
+            if len(i) > maxlen:
+                maxlen = len(i)
+        
+        solution = [""]* maxlen #That's the max our array will have to be
+
+        for word in s:
+            for j in range(maxlen):
+                if j < len(word):
+                    solution[j] += word[j]
+                else:
+                    solution[j] += " " #We do this instead of 
+
+        for j in range(len(solution)):
+            solution[j] = solution[j].rstrip()
+        
+
+        return solution
+        
+
+            
